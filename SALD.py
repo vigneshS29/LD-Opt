f1ffdfa18847b64e4e873432d4a28f230010e7fe
Vignesh Sathyaseelan <98504160+vigneshS29@users.noreply.github.com>
2023-09-01 13:55:51 -0400

    Update SALD.py

--- SALD.py
+++ SALD.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 
 def main():
     #this function returns the energy and force on a particle (Force calculated using central difference formula) 
-    #potential = lambda x: (0.005)*(np.sin(x[0]) + np.sin(x[1])) + (0.05)*(x[0]**2 + x[1]**2)
-    potential = lambda x: x[0]**2 + x[1]**2
+    potential = lambda x: (0.05)*(np.sin(x[0]) + np.sin(x[1])) + (0.0015)*(x[0]**2 + x[1]**2)
+    #potential = lambda x: x[0]**2 + x[1]**2
 
     st = time.time()
 
@@ -15,8 +15,8 @@ def main():
 
     print('Running optimization algorithm...')
 
-    times,positions,velocities = lvd(potential=potential,gradient=grad_p,initial_position=[2,8],gamma=10,p0=0.5,alpha=0.1\
-                                     ,max_time=10**6,dt=0.1,save_frequency=10)
+    times,positions,velocities = sald(potential=potential,gradient=grad_p,initial_position=[2,8],gamma=10,p0=0.5,alpha=0.1\
+                                    ,initial_temp=3*(10**25),max_anneal_cycle=20,max_time=10**5,dt=0.1,save_frequency=10)
 
     print(f'Finished in {np.round(time.time()-st,2)} seconds')
     plot_PES(potential=potential,xmin=-10,xmax=10,positions=positions)
@@ -42,60 +42,75 @@ def random_velocity_update(v,gamma,T,dt):
     v_new = c1*v + R*c2
     return v_new
 
-def lvd(potential, gradient, gamma, p0, alpha, initial_position=None, initial_velocity=None, max_time=10**5, dt = 0.01, save_frequency=10):
+def sald(potential, gradient, gamma, p0, alpha, initial_position=None, initial_velocity=None, initial_temp=3*(10**25),max_anneal_cycle=100 ,max_time=10**5, dt = 0.01, save_frequency=10):
 
-    try: os.remove('out.txt')
-    except: pass
+    anneal_step = 1
+    initial_position = initial_position
 
-    with open('out.txt','a') as f:
-        f.write('time\tX\tY\n')
-
-    t = 0
-    step_number = 0
-    positions = []
-    velocities = []
-    save_times=[]
-
-    if initial_position: x = initial_position
-    else: x = np.random.normal(10,size=2)
-
-    if initial_velocity: v = initial_velocity
-    else: v = np.random.normal(size=2)
-    
-    while t<max_time:
+    print(f'Running SALD-OPT with initial_point = {initial_position} and intitial temp {initial_temp} for {max_anneal_cycle} anneal cycles and each cycle for {max_time}')
+    while anneal_step <= max_anneal_cycle:
         
-        # B
-        potential_energy, force = potential(x),gradient(x,potential)
-        v = velocity_update(v,force,dt)
+        #set initial temp for each anneal cycle
+
+        T0 = initial_temp
+        print(f'Running anneal cycle {anneal_step}...')
+
+        try: os.remove(f'out_{anneal_step}.txt')
+        except: pass
+
+        with open(f'out_{anneal_step}.txt','a') as f:
+            f.write('time\tX\tY\n')
+
+        t = 0
+        step_number = 0
+        positions = []
+        velocities = []
+        save_times=[]
+
+        if anneal_step == 0 and initial_position == None: x = np.random.normal(10,size=2)
+        else: x = initial_position
+
+        if initial_velocity: v = initial_velocity
+        else: v = np.random.normal(size=2)
+
+        while t<max_time:
+
+            
+            # B
+            potential_energy, force = potential(x),gradient(x,potential)
+            v = velocity_update(v,force,dt)
+            
+            #A
+            x = position_update(x,v,dt)
+
+            #Temperature Anneal
+            T = T0*(alpha**step_number)
+
+            #O
+            v = random_velocity_update(v,gamma,T,dt)
+            
+            #A
+            x = position_update(x,v,dt)
+            
+            # B
+            potential_energy, force = potential(x),gradient(x,potential)
+            v = velocity_update(v,force,dt)
+
+
+            if step_number%save_frequency == 0:
+                positions += [x]
+                velocities += [v]
+                save_times += [t]
+
+                with open(f'out_{anneal_step}.txt','a') as f:
+                    f.write(f'{t:.3f}\t{x[0]:.3f}\t{x[1]:.3f}\n')
+            
+            t += dt
+            step_number += 1
         
-        #A
-        x = position_update(x,v,dt)
-
-        #Temperature Anneal
-        T0 = 3*(10**25)
-        T = T0*(alpha**step_number)
-
-        #O
-        v = random_velocity_update(v,gamma,T,dt)
-        
-        #A
-        x = position_update(x,v,dt)
-        
-        # B
-        potential_energy, force = potential(x),gradient(x,potential)
-        v = velocity_update(v,force,dt)
-
-
-        if step_number%save_frequency == 0:
-            positions += [x]
-            velocities += [v]
-            save_times += [t]
-
-            with open('out.txt','a') as f:
-                f.write(f'{t:.3f}\t{x[0]:.3f}\t{x[1]:.3f}\n')
-        
-        t += dt
-        step_number += 1
+        initial_position = x
+        anneal_step += 1
+        print(f'Anneal step {anneal_step} done. Optimized to = {initial_position}')
     
     return save_times, np.array(positions), np.array(velocities)
 
