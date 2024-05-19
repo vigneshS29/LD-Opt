71ed87ff71c75569e020029c7242a89cb6afb2c3
Vignesh Sathyaseelan <98504160+vigneshS29@users.noreply.github.com>
2024-05-19 14:54:16 -0400

    Update plot.py

--- plot.py
+++ plot.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 
 def plot_PES(potential,xmin,xmax,positions,initial_position,spacing=0.1,outname='out',savefig=True):
     
-    plt.figure(dpi=100)
+    plt.figure(figsize=(5,5))
     plt.xlim(-xmax,xmax)
     plt.ylim(-xmax,xmax)
     grid_x,grid_y = np.meshgrid(np.arange(-xmax,xmax,spacing), np.arange(-xmax,xmax,spacing))
     plt.contourf(grid_x, grid_y, potential([grid_x,grid_y]))
-    plt.scatter(initial_position[0],initial_position[1],color='black',label='Start')
-    plt.scatter(positions[:,0],positions[:,1],color='grey')
-    plt.scatter(positions[-1,0],positions[-1,1],color='red',label='End')
+    plt.scatter(positions[:,0],positions[:,1],color='grey',s=50)
+    plt.scatter(initial_position[0],initial_position[1],color='black',label='Start',edgecolor='black',s=50)
+    plt.scatter(positions[-1,0],positions[-1,1],color='red',label='End',edgecolor='black',s=50)
     plt.legend()
     if savefig: plt.savefig(f'{outname}.png')
 
@@ -21,11 +21,11 @@ def plot_PES(potential,xmin,xmax,positions,initial_position,spacing=0.1,outname=
 
 def plot_temp(times,temperature,outname='temp',savefig=True):
     
-    plt.figure(dpi=100)
+    plt.figure(figsize=(5,5))
     plt.plot(times,temperature,alpha=0.5)
     plt.title('Annealing Profile')
-    plt.ylabel('log(Temperature)')
-    plt.xlabel('log(Iteration)')
+    plt.ylabel('Temperature')
+    plt.xlabel('Iteration')
     if savefig: plt.savefig(f'{outname}.png')
 
     return
