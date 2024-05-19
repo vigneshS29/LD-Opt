b36bd3d2a138d92b8946ceaaca454a83d956d49c
Vignesh Sathyaseelan <98504160+vigneshS29@users.noreply.github.com>
2024-05-19 14:55:34 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -15,13 +15,13 @@ def main():
 
     st = time.time()
 
-    times,positions,velocities,temperature = sald(potential=potential,initial_position=initial_position,gamma=1,alpha=0.01\
-                                    ,initial_temp=10**12,max_anneal_cycle=100,max_time=5000,dt=0.1,save_frequency=10)
+    steps,positions,temperature = sald(potential=potential,initial_position=initial_position,gamma=1,alpha=0.99\
+                                    ,T=10**5,max_anneal_cycle=100,max_time=5000,dt=0.1,save_frequency=10)
 
     print(f'Finished in {np.round(time.time()-st,2)} seconds')
     
     plot_PES(potential=potential,initial_position=initial_position,xmin=-10,xmax=10,positions=positions)
-    plot_temp(times,temperature)
+    plot_temp(steps,temperature)
 
     return
 
